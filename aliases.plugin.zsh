65e720e45397932b35bde1a9b9cad4c193923837
Gustavo Tonietto <tonietto@gmail.com>
2017-02-08 22:02:35 -0200

    :sparkles: New alias aloha

--- aliases.plugin.zsh
+++ aliases.plugin.zsh
@@ -3,4 +3,4 @@ alias futuremachina='ssh gus@159.203.225.215'
 alias please='sudo'
 alias sleep='xset dpms force off' # Ubuntu specific
 alias alohaine="fortune | cowsay | lolcat"
-# alias poweroff='gnome-session-quit --power-off' # Ubuntu specific
+alias aloha="clear && figlet ALOHA! | lolcat"
