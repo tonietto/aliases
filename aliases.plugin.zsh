d8cbd01edc2ff39a46d9b4f356f743c594aa4163
Gustavo Tonietto <tonietto@gmail.com>
2017-02-10 10:02:52 -0200

    :ok_hand: Update suspend alias name
    - Due to conflict with bash's `sleep` command

--- aliases.plugin.zsh
+++ aliases.plugin.zsh
@@ -1,6 +1,6 @@
 # aliases
 alias futuremachina='ssh gus@159.203.225.215'
 alias please='sudo'
-alias sleep='xset dpms force off' # Ubuntu specific
+alias suspend='xset dpms force off' # Ubuntu specific
 alias alohaine="fortune | cowsay | lolcat"
 alias aloha="clear && figlet ALOHA! | lolcat"
