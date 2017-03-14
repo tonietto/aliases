0efc8e927ac21105c046291a1ddf2cb4fbf9fa81
Gustavo Tonietto <tonietto@gmail.com>
2017-03-14 17:05:24 -0300

    :wrench: Update aliases

--- aliases.plugin.zsh
+++ aliases.plugin.zsh
@@ -1,5 +1,6 @@
 # aliases
-alias futuremachina='ssh gus@159.203.225.215'
+alias futuremachina-mki='ssh gus@159.203.225.215'
+alias futuremachina='ssh codiane@104.236.12.204'
 alias please='sudo'
 alias suspend='xset dpms force off' # Ubuntu specific
 alias alohaine="fortune | cowsay | lolcat"
