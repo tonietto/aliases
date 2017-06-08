828c853a1866c42e4149b85caebfe2706e1adb11
Gustavo Tonietto <tonietto@gmail.com>
2017-06-08 10:51:22 -0300

    :sparkles: Add new Tmux and convenience aliases

--- aliases.plugin.zsh
+++ aliases.plugin.zsh
@@ -2,9 +2,24 @@
 alias futureagenda='ssh root@198.199.92.54'
 alias futuremachina-mki='ssh gus@159.203.225.215'
 alias futuremachina='ssh root@45.55.113.171'
-alias please='sudo'
+
+# suspend and exit
 alias suspend='xset dpms force off' # Ubuntu specific
+alias bye="clear && figlet BYE! | lolcat && revolver -s arrow2 start && sleep 5 && exit"
+
+# "bobiçada"
 alias alohaine="fortune | cowsay | lolcat"
 alias aloha="clear && figlet ALOHA! | lolcat"
 alias showip="ipconfig getifaddr en0"
-alias bye="clear && figlet BYE! | lolcat && revolver -s arrow2 start && sleep 5 && exit"
+
+# aliases for Tmux
+alias tmux='tmux -2'
+alias ta='tmux attach -t'
+alias tnew='tmux new -s'
+alias tls='tmux ls'
+alias tkill='tmux kill-session -t'
+
+# convenience aliases for editing configs
+alias ev='vim ~/.vimrc'
+alias et='vim ~/.tmux.conf'
+alias ez='vim ~/.zshrc'
