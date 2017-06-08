a026a9865659b85fc35a8acf52e1c3c421edb2e2
Gustavo Tonietto <tonietto@gmail.com>
2017-06-08 11:18:44 -0300

    :sparkles: Add new edit hyper alias

--- aliases.plugin.zsh
+++ aliases.plugin.zsh
@@ -20,6 +20,7 @@ alias tls='tmux ls'
 alias tkill='tmux kill-session -t'
 
 # convenience aliases for editing configs
+alias eh='vim ~/.hyper.js'
 alias ev='vim ~/.vimrc'
 alias et='vim ~/.tmux.conf'
 alias ez='vim ~/.zshrc'
