8e69672cd86bee56e8384ba47f213f68c39bd2a6
Gustavo Tonietto <tonietto@gmail.com>
2017-08-31 11:54:02 -0300

    :lipstick: Rename cspad to cn

--- aliases.plugin.zsh
+++ aliases.plugin.zsh
@@ -5,7 +5,7 @@ alias ra='ranger'
 # terminal
 alias c='clear'
 alias cs='clear && show-version'
-alias cspad='cd ~ && clear && figlet NOTES'
+alias cn='cd ~ && clear && figlet NOTES'
 alias cb='xclip -selection clipboard'
 
 # suspend and exit
