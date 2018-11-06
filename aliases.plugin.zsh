0106ae63c651f9c086e470ccd11ee440043f9f9e
Gustavo Tonietto <tonietto@gmail.com>
2018-11-06 15:17:40 -0200

    Add ssh alias

--- aliases.plugin.zsh
+++ aliases.plugin.zsh
@@ -63,6 +63,7 @@ alias vat='vimcat'
 alias pue='pass unlock && exit'
 alias pul='pass unlock && encfs -i 60 ~/.ledger ~/ledger'
 alias pu='pass unlock'
+alias ssh='kiss && ssh'
 #}}}
 # Suspend and exit {{{
 alias suspend='xset dpms force off' # Ubuntu specific
